88edc7d1192c46e887549ef76b7f461e6a557c80
shaitools891-svg <shaitools891-svg@github.com>
2026-03-03 14:39:25 +0600

    new

--- src/components/MCQTest.tsx
+++ src/components/MCQTest.tsx
@@ -419,7 +419,7 @@ export default function MCQTest() {
   };
 
   useEffect(() => {
-    let interval: NodeJS.Timeout;
+    let interval: ReturnType<typeof setInterval>;
     if (isTimerRunning && !showResults) {
       interval = setInterval(() => {
         setTimeElapsed(prev => prev + 1);
